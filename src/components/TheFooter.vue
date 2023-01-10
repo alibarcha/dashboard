045e9bf7934c68650542e0132b806de28eee1ab2
ali haider <alibarcha910@gmail.com>
2023-01-10 05:36:26 -0800

    create footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,16 +1,56 @@
 <template>
-  <div>
-    footer
-  </div>
+  <v-footer tile padless app>
+    <v-container>
+      <v-row>
+        <v-col md="6" cols="12">
+          <a
+            target="_blank"
+            href="#"
+            class="grey--text text--darken-3 text-decoration-none ps-5"
+            >alibarcha</a
+          >
+          <a
+            target="_blank"
+            href="#"
+            class="grey--text text--darken-3 text-decoration-none ps-5"
+            >ABOUT
+          </a>
+          <a
+            target="_blank"
+            href="#"
+            class="grey--text text--darken-3 text-decoration-none ps-5"
+          >
+            BLOG
+          </a>
+          <a
+            target="_blank"
+            href="#"
+            class="grey--text text--darken-3 text-decoration-none ps-5"
+            >LICENSES</a
+          >
+        </v-col>
+        <v-col md="6" cols="12" class="text-sm-end"
+          >© 2022, made with
+          <v-icon>mdi-heart-multiple</v-icon>
+          by
+          <a
+            href="#"
+            class="text-decoration-none success--text font-weight-light"
+          >
+            alibarcha
+          </a>
+          for a better web.</v-col
+        >
+      </v-row>
+    </v-container>
+  </v-footer>
 </template>
 
 <script>
 export default {
-    name:'TheFooter',
-
-}
+  name: "TheFooter",
+};
 </script>
 
 <style>
-
 </style>
