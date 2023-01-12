83bb9fcaf0545c76170e4b31f0b86c8fae1e673f
ali haider <alibarcha910@gmail.com>
2023-01-12 06:49:58 -0800

    creat cards in dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -144,36 +144,168 @@
     <!-- Main Cards -->
 
     <v-row class="mb-10" style="border: 1px solid red">
-
       <v-col lg="6" cols="12">
-
         <MainCard SheetColor="warning">
           <template #sheetData>
-            <div class="title font-weight-light"> Employees Stats </div>
-            <div class="subtitle-2 font-weight-light " style="color: hsla(0,0%,100%,.8);"> New employees on 15th September, 2016 </div>
-
+            <div class="title font-weight-light">Employees Stats</div>
+            <div
+              class="subtitle-2 font-weight-light"
+              style="color: hsla(0, 0%, 100%, 0.8)"
+            >
+              New employees on 15th September, 2022
+            </div>
           </template>
-           <template #cardData>
-            cardData
+          <template #cardData>
+            <!-- cardData table -->
+            <v-data-table
+              :headers="headers"
+              :items="tableData"
+              :items-per-page="5"
+              class="elevation-0 font-weight-light"
+            ></v-data-table>
           </template>
         </MainCard>
-
       </v-col>
 
-       <v-col lg="6" cols="12" class="mt-lg-0 mt-12">
-
-          <MainCard SheetColor="success">
+      <v-col lg="6" cols="12" class="mt-lg-0 mt-12">
+        <MainCard SheetColor="success">
           <template #sheetData>
-            sheetData
+            <div class="sheetMainCardWrapper">
+              <span class="title font-weight-light pl-sm-0 pl-5">Tasks:</span>
+              <!-- tabs -->
+              <v-tabs
+                v-model="tab"
+                align-with-title
+                background-color="transparent"
+                dark
+                show-arrows
+                centered
+              >
+                <v-tabs-slider color="white"></v-tabs-slider>
+                <v-tab v-for="(item, index) in items" :key="`item${index}`">
+                  <v-icon class="mr-2">{{ item.icon }}</v-icon>
+                  {{ item.text }}
+                </v-tab>
+              </v-tabs>
+            </div>
           </template>
-           <template #cardData>
-            cardData
+
+          <template #cardData>
+            <!-- tabs content -->
+            <v-tabs-items v-model="tab">
+              <!-- 1 tab content -->
+              <v-tab-item>
+                <v-list>
+                  <v-list-item
+                    v-for="(tab, index) in tabContent1"
+                    v-bind:key="`${index}`"
+                  >
+                    <template v-slot:default="{ active }">
+                      <v-list-item-action>
+                        <v-checkbox
+                          :input-value="active"
+                          color="success"
+                        ></v-checkbox>
+                      </v-list-item-action>
+                      <v-list-item-content>
+                        <v-row>
+                          <v-col sm="10" cols="12">
+                            <v-list-item-title
+                              class="font-weight-light cardDataPara"
+                              >{{ tab.text }}</v-list-item-title
+                            >
+                          </v-col>
+                          <v-col cols="1">
+                            <v-icon size="20">mdi-pencil</v-icon>
+                          </v-col>
+                          <v-col cols="1">
+                            <v-icon class="error--text" size="20"
+                              >mdi-close</v-icon
+                            >
+                          </v-col>
+                        </v-row>
+                      </v-list-item-content>
+                    </template>
+                  </v-list-item>
+                </v-list>
+              </v-tab-item>
+              <!-- 2 ta content -->
+              <v-tab-item>
+                <v-list>
+                  <v-list-item
+                    v-for="(tab, index) in tabContent2"
+                    v-bind:key="`${index}`"
+                  >
+                    <template v-slot:default="{ active }">
+                      <v-list-item-action>
+                        <v-checkbox
+                          :input-value="active"
+                          color="success"
+                        ></v-checkbox>
+                      </v-list-item-action>
+                      <v-list-item-content>
+                        <v-row>
+                          <v-col sm="10" cols="12">
+                            <v-list-item-title
+                              class="font-weight-light cardDataPara"
+                              >{{ tab.text }}</v-list-item-title
+                            >
+                          </v-col>
+                          <v-col cols="1">
+                            <v-icon size="20">mdi-pencil</v-icon>
+                          </v-col>
+                          <v-col cols="1">
+                            <v-icon class="error--text" size="20"
+                              >mdi-close</v-icon
+                            >
+                          </v-col>
+                        </v-row>
+                      </v-list-item-content>
+                    </template>
+                  </v-list-item>
+                </v-list>
+              </v-tab-item>
+
+              <!-- 3 tab content -->
+              <v-tab-item>
+                <v-list>
+                  <v-list-item
+                    v-for="(tab, index) in tabContent3"
+                    v-bind:key="`tab${index}`"
+                  >
+                    <template v-slot:default="{ active }">
+                      <v-list-item-action>
+                        <v-checkbox
+                          :input-value="active"
+                          color="success"
+                        ></v-checkbox>
+                      </v-list-item-action>
+                      <v-list-item-content>
+                        <v-row>
+                          <v-col sm="10" cols="12">
+                            <v-list-item-title
+                              class="font-weight-light cardDataPara"
+                              >{{ tab.text }}</v-list-item-title
+                            >
+                          </v-col>
+                          <v-col cols="1">
+                            <v-icon size="20">mdi-pencil</v-icon>
+                          </v-col>
+                          <v-col cols="1">
+                            <v-icon class="error--text" size="20"
+                              >mdi-close</v-icon
+                            >
+                          </v-col>
+                        </v-row>
+                      </v-list-item-content>
+                    </template>
+                  </v-list-item>
+                </v-list>
+              </v-tab-item>
+            </v-tabs-items>
           </template>
         </MainCard>
-
-
       </v-col>
-     
     </v-row>
   </v-container>
 </template>
@@ -223,6 +355,100 @@ export default {
           },
         ],
       },
+
+      //-------card table data ---------
+      headers: [
+        {
+          text: "ID",
+          align: "start",
+          sortable: false,
+          value: "id",
+        },
+        { text: "Name", value: "name" },
+        { text: "Salary", value: "salary" },
+        { text: "Country", value: "country" },
+        { text: "City", value: "city" },
+      ],
+      tableData: [
+        {
+          name: "alibarcha",
+          id: 1,
+          salary: "$35,738",
+          country: "pakistan",
+          city: "islamabad",
+        },
+        {
+          name: "Minerva Hooper",
+          id: 2,
+          salary: "$23,738",
+          country: "Curaçao	",
+          city: "Sinaai-Waas",
+        },
+        {
+          name: "Sage Rodriguez",
+          id: 3,
+          salary: "$56,142",
+          country: "Netherlands",
+          city: "Overland Park",
+        },
+        {
+          name: "Philip Chanley",
+          id: 4,
+          salary: "$35,738",
+          country: "Korea, South",
+          city: "Gloucester",
+        },
+        {
+          name: "Doris Greene",
+          id: 5,
+          salary: "$65,738",
+          country: "Malawi",
+          city: "Feldkirchen in Kārnten",
+        },
+      ],
+      // --------tabs Data --------
+      tab: null,
+      items: [
+        { text: "Bugs", icon: "mdi-bug" },
+        { text: "WEBSITE", icon: "mdi-code-tags" },
+        { text: "SERVER", icon: "mdi-cloud" },
+      ],
+      // tab content 1
+      tabContent1: [
+        {
+          text: 'Sign contract for "What are conference organizers afraid of?',
+        },
+        {
+          text: "Lines From Great Russian Literature? Or E-mails From My Boss?",
+        },
+        {
+          text: "Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit",
+        },
+        {
+          text: 'Sign contract for "What are conference organizers afraid of?',
+        },
+      ],
+      // tab content 2
+      tabContent2: [
+        {
+          text: 'Sign contract for "What are conference organizers afraid of?',
+        },
+        {
+          text: "Lines From Great Russian Literature? Or E-mails From My Boss?",
+        },
+      ],
+      // tab content 3
+      tabContent3: [
+        {
+          text: 'Sign contract for "What are conference organizers afraid of?',
+        },
+        {
+          text: "Lines From Great Russian Literature? Or E-mails From My Boss?",
+        },
+        {
+          text: "Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit",
+        },
+      ],
     };
   },
 };
@@ -233,6 +459,15 @@ export default {
   top: -24px;
   position: relative;
   border-radius: 5px;
+}
+/* sheetMainCardWrapper */
+.sheetMainCardWrapper {
+  display: flex;
+  justify-content: center !important;
+  align-items: center;
+}
+.cardDataPara {
+  white-space: pre-wrap;
 }
 </style>
 
